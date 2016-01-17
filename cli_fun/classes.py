dcc12570524e7699362aa41c5dc5ec698a958449
Nate Tangsurat <e4r7hbug@gmail.com>
2016-01-16 19:02:42 -0600

    FunCLI: Use class level shared logger

--- cli_fun/classes.py
+++ cli_fun/classes.py
@@ -30,49 +30,47 @@ class Context(object):
 
 class FunCLI(click.MultiCommand):
     """Click class for gathering commands."""
+    log = logging.getLogger(__name__)
 
     def list_commands(self, ctx):
         """Search through the _commands_ directory for modules to use."""
-        log = logging.getLogger(__name__)
-
         command_list = []
         cmd_folder = os.path.abspath(os.path.join(
             os.path.dirname(__file__), 'commands'))
 
         for filename in os.listdir(cmd_folder):
-            log.debug('Found file in command directory: %s', filename)
+            self.log.debug('Found file in command directory: %s', filename)
 
             if filename.endswith('.py'):
                 if not filename.startswith('__'):
                     command_name = filename[0:-3]
 
-                    log.debug('Adding command to list: %s', command_name)
+                    self.log.debug('Adding command to list: %s', command_name)
                     command_list.append(command_name)
 
         command_list.sort()
-        log.debug('Sorted command list: %s', command_list)
+        self.log.debug('Sorted command list: %s', command_list)
         return command_list
 
     def get_command(self, ctx, name):
         """Dynamically import modules in the _commands_ directory."""
-        log = logging.getLogger(__name__)
-
         try:
             if sys.version_info[0] == 2:
-                log.debug('Python 2 detected, encoding "%s" in ascii.', name)
+                self.log.debug('Python 2 detected, encoding "%s" in ascii.',
+                               name)
                 name = name.encode('ascii', 'replace')
 
             mod = importlib.import_module('.commands.{0}'.format(name),
                                           __package__)
-            log.debug('Imported module: %s', mod)
+            self.log.debug('Imported module: %s', mod)
 
             return mod.cli
         except ImportError as error:
-            log.warning('Failed to import: %s', name)
-            log.warning('Error information:\n%s', error)
+            self.log.warning('Failed to import: %s', name)
+            self.log.warning('Error information:\n%s', error)
             return
         except SyntaxError:
-            log.warning('Failed to import: %s', name)
-            log.warning('Might be a Python %s incompatible module.',
-                        sys.version_info[0])
+            self.log.warning('Failed to import: %s', name)
+            self.log.warning('Might be a Python %s incompatible module.',
+                             sys.version_info[0])
             return
