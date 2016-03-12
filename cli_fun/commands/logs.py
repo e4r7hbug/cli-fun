78282f4f5c176da96e53dcd67a0b760e579584d1
Nate Tangsurat <e4r7hbug@gmail.com>
2016-03-12 01:50:44 -0600

    logs: Make format string a little more dynamic

--- cli_fun/commands/logs.py
+++ cli_fun/commands/logs.py
@@ -13,11 +13,25 @@ def cli():
     root_log.setLevel(logging.DEBUG)
 
     root_handler = logging.StreamHandler()
-    root_formatter = logging.Formatter(
-        '%(name)s\t%(levelno)s\t%(pathname)s\t%(filename)s\t%(module)s\t'
-        '%(lineno)d\t%(funcName)s\t%(created)f\t%(asctime)s\t%(msecs)d\t'
-        '%(relativeCreated)d\t%(thread)d\t%(threadName)s\t%(process)d\t'
-        '%(message)s')
+    root_formatter = logging.Formatter('\t'.join(['%({0}){1}'.format(item,
+                                                                     type)
+                                                  for item, type in {
+                                                      'name': 's',
+                                                      'levelno': 's',
+                                                      'pathname': 's',
+                                                      'filename': 's',
+                                                      'module': 's',
+                                                      'lineno': 'd',
+                                                      'funcName': 's',
+                                                      'created': 'f',
+                                                      'asctime': 's',
+                                                      'msecs': 'd',
+                                                      'relativeCreated': 'd',
+                                                      'thread': 'd',
+                                                      'threadName': 's',
+                                                      'process': 'd',
+                                                      'message': 's'
+                                                  }.items()]))
     root_handler.setFormatter(root_formatter)
 
     root_log.addHandler(root_handler)
