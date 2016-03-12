6f02c9f0f488eef10deb11e1df572b8d88001aec
Nate Tangsurat <e4r7hbug@gmail.com>
2016-03-12 01:54:11 -0600

    logs: Pull format string components out

--- cli_fun/commands/logs.py
+++ cli_fun/commands/logs.py
@@ -12,26 +12,28 @@ def cli():
     root_log = logging.root
     root_log.setLevel(logging.DEBUG)
 
+    format_dict = {
+        'name': 's',
+        'levelno': 's',
+        'pathname': 's',
+        'filename': 's',
+        'module': 's',
+        'lineno': 'd',
+        'funcName': 's',
+        'created': 'f',
+        'asctime': 's',
+        'msecs': 'd',
+        'relativeCreated': 'd',
+        'thread': 'd',
+        'threadName': 's',
+        'process': 'd',
+        'message': 's'
+    }
+    format_string = '\t'.join(['%({0}){1}'.format(item, type)
+                               for item, type in format_dict.items()])
+
     root_handler = logging.StreamHandler()
-    root_formatter = logging.Formatter('\t'.join(['%({0}){1}'.format(item,
-                                                                     type)
-                                                  for item, type in {
-                                                      'name': 's',
-                                                      'levelno': 's',
-                                                      'pathname': 's',
-                                                      'filename': 's',
-                                                      'module': 's',
-                                                      'lineno': 'd',
-                                                      'funcName': 's',
-                                                      'created': 'f',
-                                                      'asctime': 's',
-                                                      'msecs': 'd',
-                                                      'relativeCreated': 'd',
-                                                      'thread': 'd',
-                                                      'threadName': 's',
-                                                      'process': 'd',
-                                                      'message': 's'
-                                                  }.items()]))
+    root_formatter = logging.Formatter(format_string)
     root_handler.setFormatter(root_formatter)
 
     root_log.addHandler(root_handler)
